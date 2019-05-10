661b503cc6ec14d4dcb54273bf21ee07ab637db0
Darmawantz <43660766+Darmawantz@users.noreply.github.com>
2019-05-10 13:41:27 +0700

    Add files via upload

---
+++ StarWars0009.java
@@ -0,0 +1,26 @@
+package sembarang;
+
+import java.util.Scanner;
+
+public class StarWars0009 {
+	public static void main(String[] args) {
+		// Nama 	: Darmawan;
+		// NIM		: D0217504;
+		// Kelas	: TI.A 2017;
+		Scanner sIchsan = new Scanner (System.in);
+		System.out.println("input ");
+		int a = sIchsan.nextInt();
+		
+		System.out.println("output ");
+		for (int i=0; i<=a;i++) {
+			for(int j=i; j<a;j++) {
+				System.out.print("  ");
+			}	
+				for(int j=1; j<=i;j++) {
+			System.out.print("X ");
+		}
+			System.out.println();
+		}
+
+	}
+}
